cd10b5af6bc33d97a1b66658a32860a9e814ec15
goshawke <52211392+goshawke@users.noreply.github.com>
2022-04-16 21:07:39 -0400

    Update registers.py

--- CPU/registers.py
+++ CPU/registers.py
@@ -1,30 +1,46 @@
-from Memory.memory import *
-from Memory.cache import *
 
 class Register:
+    """This class is the super class of all of registers
+    Parameters:
+    --------------
+    size : int type; the size of the register
+    value: str type; the value of the register
+    label: str type; the kind of the register
+    """
 
-    def __init__(self, size=0, label=''):
+    def __init__(self, size=4, label='register'):
         self.size = size
         self.value = '0' * self.size
         self.label = label
 
     def check_state(self, input_value):
         """This function checks if the value cause fault"""
-        max = int('1'*self.size, 2)
-        min = int('0'*self.size, 2)
-        if input_value > max:
+        max_value = int('1' * self.size, 2)
+        min_value = int('0' * self.size, 2)
+        if input_value > max_value:
             return 'OVERFLOW'
-        elif input_value < min:
+        elif input_value < min_value:
             return 'UNDERFLOW'
         else:
             return '0000'
 
-
-    def check_overflow(self, input_value) -> bool:
-
-        return True if len(input_value) > self.size else False
+    def add_10(self, adder: int):
+        """This function adds value and return the state of the register
+        Parameters:
+        ------------
+        adder: the decimal number that going to be added to
+        """
+        value = int(self.value, 2) + adder
+        if value >= 0:
+            temp = bin(value)[2:].zfill(self.size)
+        else:
+            temp = bin(value)[3:].zfill(self.size)
+        state = self.check_state(value)
+        self.set_value(str(int(temp[-self.size:])))
+        return state
 
     def get_value(self):
+        """Return str type of decimal value"""
         return self.value
 
     def set_value(self, val: str):
@@ -33,84 +49,70 @@ class Register:
 
         self.value = val
 
-    # TODO - delete?
-    def add_2(self, adder: str):
-
-        temp = bin(int(self.value, 2) + int(adder, 2))[2:]
-        if self.check_overflow(temp) != True:
-            self.set_value(temp)
-        else:
-            print(self.label + ' overflow error')
-
-    # TODO
-    def add_10(self, adder: int):
-        """This function adds value and return the state of the register
-        Parameters:
-        ------------
-        adder: the decimal number that going to be added to
-        """
-        value = int(self.value,2) + adder
-        if value >= 0:
-            temp = bin(value)[2:].zfill(self.size)
-        else:
-             temp = bin(value)[3:].zfill(self.size)
-        state = self.check_state(value)
-        self.set_value(str(int(temp[-self.size:])))
-
-        return state
-
-
     def reset(self):
+        """This function resets the register"""
         self.value = '0' * self.size
 
 
 class PC(Register):
+    """This is the class of Program Counter
+    PC has 12 bits
+    """
 
     def __init__(self, size=12, label='PC'):
         super().__init__(size=size, label=label)
 
     def next(self):
+        """This function defines how pc find the next instruction"""
         self.add_10(1)
 
 
 class MAR(Register):
+    """This is the class of Memory Address Register
+    MAR has 12 bits
+
+    Function:
+    ------------
+    get_from_PC
+    """
 
     def __init__(self, size=12, label='MAR'):
         super().__init__(size=size, label=label)
 
     def get_from_PC(self, pc: PC):
-        self.value = pc.get_value()
+        """The function for MAR <- PC
+        """
+        self.value = pc.value
 
 
 class MBR(Register):
+    """This is the class of Memory Buffer Register
+    MBR has 16 bits
+    """
 
     def __init__(self, size=16, label='MBR'):
         super().__init__(size=size, label=label)
 
-    # TODO - delete?
-    '''
-    def load_from_mem(self, mar: MAR, mem: Memory):
-        address = int(''.join(mar.value), 2)
-        self.value = mem.get_from_memory(address)
 
-        while len(self.value) < self.size:
-            self.value = '0' + self.value
-    '''
-    # TODO - delete?
-    '''
-    def store_to_mem(self, mar: MAR, mem: Memory):
-        address = int(''.join(mar.value), 2)
+class IR(Register):
+    """This is the class of Instruction Register
+    IR has 16 bits
+    """
 
-        while len(self.value) < 16:
-            self.value = '0' + self.value
+    def __init__(self, size=16, label='IR'):
+        super().__init__(size=size, label=label)
 
-        mem.set_to_memory(address, self.value)
-    '''
+    def get_from_MBR(self, mbr: MBR):
+        """The function for IR <- MBR
+        """
+        self.value = mbr.value
+
 
 class CC(Register):
     """This is the class of Condition Code
     CC has 4 bits
     """
+
     def __init__(self, size=4, label='CC'):
         super().__init__(size=size, label=label)
         self.state = '0000'
@@ -119,7 +121,7 @@ class CC(Register):
         self.value = '0' * self.size
         self.state = '0000'
 
-    def set_state(self, state : str):
+    def set_state(self, state: str):
         self.reset()
         self.state = state
         value = list(self.value)
@@ -134,33 +136,28 @@ class CC(Register):
         self.value = ''.join(value)
 
 
-class IR(Register):
-
-    def __init__(self, size=16, label='IR'):
-        super().__init__(size=size, label=label)
-
-    def get_from_MBR(self, mbr: MBR):
-        self.value = mbr.value
-
-
 class MFR(Register):
+    """This is the class of Memory Fault Register
+    MFR has 4 bits
+    """
 
     def __init__(self, size=4, label='MFR'):
         super().__init__(size=size, label=label)
 
 
 class GPR(Register):
+    """This is the class of General Purpose Register
+    GPR has 16 bits
+    """
 
     def __init__(self, size=16, label='GPR'):
         super().__init__(size=size, label=label)
 
 
 class IXR(Register):
+    """This is the class of Index Register
+    IXR has 16 bits
+    """
 
     def __init__(self, size=16, label='IXR'):
-        super().__init__(size=size, label=label)
-
-class PRIV(Register):
-
-    def __init__(self, size=1, label='PRIV'):
         super().__init__(size=size, label=label)
