a31265db27da7c07bf9984ca8aad7739d22173da
goshawke <52211392+goshawke@users.noreply.github.com>
2022-04-16 21:07:02 -0400

    Update ALU.py

--- CPU/ALU.py
+++ CPU/ALU.py
@@ -1,6 +1,7 @@
 
 from CPU.registers import *
 
+
 class ALU:
     def __init__(self, cc):
         self.irr = Register(16, 'IRR')
@@ -11,16 +12,15 @@ class ALU:
         self.irr = Register(16, 'IRR')
         self.value = 0
 
-    def arithmetic_cal(self, operation : str, o1 : str, o2 : str):
+    def arithmetic_cal(self, operation: str, o1: str, o2: str):
         """This function does arithmetic calculation
         operation can be one of:
         '+', '-', '*', '/', '%'
         """
         self.irr.reset()
         self.value = 0
-        state = '0000'
-        o1_value = int(o1,2)
-        o2_value = int(o2,2)
+        o1_value = int(o1, 2)
+        o2_value = int(o2, 2)
         if operation == '+':
             self.value = o1_value + o2_value
         elif operation == '-':
@@ -37,20 +37,19 @@ class ALU:
         self.cc.set_state(state)
         return self.irr.value
 
-    def logic_cal(self, operation : str, o1 : str, o2 = ''):
+    def logic_cal(self, operation: str, o1: str, o2=''):
         """This function does logical calculation
         operation can be one of:
         '&', '|', '~'
         """
         self.irr.reset()
         self.value = 0
-        state = '0000'
         if operation == '&':
-            self.value = int(o1,2) & int(o2,2)
+            self.value = int(o1, 2) & int(o2, 2)
         elif operation == '|':
-            self.value = int(o1,2) | int(o2,2)
+            self.value = int(o1, 2) | int(o2, 2)
         elif operation == '~':
-            #self.value = ~ o1_value
+            # self.value = ~ o1_value
             value = []
             for i in list(o1):
                 if i == '1':
@@ -58,45 +57,43 @@ class ALU:
                 else:
                     value.append('1')
             value = ''.join(value)
-            self.value = int(value,2)
+            self.value = int(value, 2)
         # check OVERFLOW or UNDERFLOW
         state = self.irr.add_10(self.value)
         self.cc.set_state(state)
         return self.irr.value
 
-    def shift(self, value : str, count, l_r, a_l):
+    def shift(self, value: str, count, l_r, a_l):
         """This function does shift"""
         self.irr.reset()
         self.value = 0
-        state = '0000'
         # left
         if l_r == 1:
-            temp = '0'*count
+            temp = '0' * count
             value += temp
         # right
         else:
             # logically
             if a_l == 1:
-                temp = '0'*count
+                temp = '0' * count
                 length = len(value)
                 value = temp + value
                 value = value[:length]
             # arithmetically
             else:
-                temp = value[0]*count
+                temp = value[0] * count
                 length = len(value)
                 value = temp + value
                 value = value[:length]
-        self.value = int(value,2)
+        self.value = int(value, 2)
         state = self.irr.add_10(self.value)
         self.cc.set_state(state)
         return self.irr.value
 
-    def rotate(self, value : str, count, l_r, a_l):
+    def rotate(self, value: str, count, l_r, a_l):
         """This function does rotate"""
         self.irr.reset()
         self.value = 0
-        state = '0000'
         temp = value
         print(temp)
         while count > 0:
@@ -112,7 +109,7 @@ class ALU:
             count -= 1
             print(count, value)
         print(value)
-        self.value = int(value,2)
+        self.value = int(value, 2)
         state = self.irr.add_10(self.value)
         self.cc.set_state(state)
         return self.irr.value
