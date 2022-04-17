92405cab4937c3a58e2dddde0200eb3c6d0dd7df
goshawke <52211392+goshawke@users.noreply.github.com>
2022-04-16 21:08:42 -0400

    Update ioDevice.py

--- ioDevice.py
+++ ioDevice.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 
 class IO:
     def __init__(self, id):
@@ -6,6 +9,7 @@ class IO:
 
     def reset(self):
         self.content = ''
+
 
 class Keyboard(IO):
     def __init__(self, id=0):
@@ -26,11 +30,12 @@ class Keyboard(IO):
         if len(number) == 0:
             return False
         else:
-            self.content = int(number)
+            self.content = number
             return True
 
     def read(self):
         return self.content
+
 
 class Printer(IO):
     def __init__(self, id=1):
@@ -39,9 +44,18 @@ class Printer(IO):
         self.line_id = 1
 
     def write_line(self, new_line):
+        if int(new_line) > 10:
+            new_line = chr(int(new_line))
         self.content_line = new_line
         self.content += 'line ' + str(self.line_id) + ':\t' + self.content_line + '\n'
         self.line_id += 1
+
+    # new - not sure if correct
+    def write_paragraph(self, new_paragraph):
+        self.content_line = new_paragraph
+        self.content += 'line ' + str(self.line_id) + ':\t' + self.content_line + '\n'
+        self.line_id += 1
+    # new
 
     def read_content(self):
         return self.content
@@ -53,3 +67,50 @@ class Printer(IO):
         self.content_line = ''
         self.content = ''
         self.line_id = 1
+
+# new - not sure if correct
+class Reader(IO):
+    def __init__(self, id=2):
+        super().__init__(id=id)
+        self.flag = None
+        self.msg = None
+        self.content = None
+
+    def reset(self):
+        self.content = None
+        self.flag = None
+        self.msg = None
+
+
+    def read_file(self, text_dir):
+
+            def resource_path(relative_path):
+                """ Get the absolute path to the resource, works for dev and for PyInstaller """
+                try:
+                    # PyInstaller creates a temp folder and stores path in _MEIPASS
+                    base_path = sys._MEIPASS
+                except Exception:
+                    base_path = os.path.abspath(".")
+
+                return os.path.join(base_path, relative_path)
+
+            #   END: resource_path
+
+            try:
+                with open(resource_path(text_dir), 'r') as f:
+                    self.content = f.readlines()
+                    self.flag = '1'
+                    self.msg = "Text File Opened Successfully"
+                f.close()
+            except FileNotFoundError:
+                print(text_dir + ' does not exist')
+                self.flag = '0'
+                return
+
+            # for i in lines:
+                # self.content += i
+
+
+
+
+
